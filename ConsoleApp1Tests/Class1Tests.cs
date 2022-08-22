66e3930c9e917a469fe2124abdcb8dcf94a64d6e
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-08-22 23:00:28 +0800

    Add project files.

---
+++ ConsoleApp1Tests/Class1Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1.Tests
+{
+    using ConsoleApp1.Fakes;
+    using Microsoft.QualityTools.Testing.Fakes;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void HelloTest()
+        {
+            using (ShimsContext.Create())
+            {
+                var i = 0;
+                ShimClass1.AllInstances.HelloString = (sender, whom) =>
+                {
+                    i++;
+                    //sender.Hello(who);
+                    Console.WriteLine($"Good Bye {whom}");
+                };
+                var c1 = new Class1();
+                c1.Hello("world");
+
+            }
+
+        }
+    }
+}
