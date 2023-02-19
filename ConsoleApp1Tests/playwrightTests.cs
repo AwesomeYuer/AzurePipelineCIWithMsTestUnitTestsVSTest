800517227243c9bd43e49b466954720e09b631a5
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-02-19 17:51:19 +0800

    browserChannel

--- ConsoleApp1Tests/playwrightTests.cs
+++ ConsoleApp1Tests/playwrightTests.cs
@@ -20,16 +20,53 @@
                 throw new Exception($"Playwright exited with code {exitCode}");
             }
         }
+
+        [DataRow("msedge")]
+        [DataRow("chrome")]
         [TestMethod()]
-        public async Task Baidu_Test()
+        public async Task Baidu_Test(string browserChannel)
         {
             var playwright = await Playwright.CreateAsync();
-            await using var browser = await playwright.Chromium.LaunchAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true, Channel = browserChannel });
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://www.baidu.com");
             var title = await page.InnerTextAsync("title");
             Console.WriteLine(title);
             Assert.IsTrue(title.Contains("百度"));
+        }
+
+
+        //[TestMethod()]
+        public async Task BaiduSearch_Test()
+        {
+
+            var playwright = await Playwright.CreateAsync();
+            //await using var browser = await playwright.Chromium.LaunchAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 1000, Channel = "chrome", ChromiumSandbox = true });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://www.baidu.com");
+            var title = await page.InnerTextAsync("title");
+            Console.WriteLine(title);
+            Assert.IsTrue(title.Contains("百度"));
+
+            //var playwright = await Playwright.CreateAsync();
+            //var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 1000, Channel = "chrome", ChromiumSandbox = true });
+
+            //var page = await browser.NewPageAsync();
+            //await page.GotoAsync("https://www.baidu.com");
+
+            //await page.Locator("id=kw").FillAsync(Guid.NewGuid().ToString());
+
+            //await page.Locator("id=su").ClickAsync();
+
+            //await Task.Delay(2000);
+
+            //await page.Locator("//*[@id=\"u\"]/a[2]").HoverAsync();
+
+            //await page.Locator("//*[@id=\"u\"]/div/a[1]/span").ClickAsync();
+            //await page.Locator("id=sh_1").CheckAsync();
+            //var a = page.GetByText("抱歉");
+
         }
     }
 }
