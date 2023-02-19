c0349722f0e0c48fe11cba80091b0e103b09119b
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-02-19 16:22:50 +0800

    Baidu_Test

---
+++ ConsoleApp1Tests/playwrightTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1.Tests
+{
+    using Microsoft.Playwright;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass(), TestCategory(nameof(PlaywrightTests))]
+    public class PlaywrightTests
+    {
+        [TestMethod()]
+        public async Task Baidu_Test()
+        {
+            var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://www.baidu.com");
+            var title = await page.InnerTextAsync("title");
+            Console.WriteLine(title);
+            Assert.IsTrue(title.Contains("百度"));
+        }
+    }
+}
